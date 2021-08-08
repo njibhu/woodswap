4c440c7cf7d9b64bd1dfa5c4ccd4f0cc9b845a66
Njibhu <manu@njibhu.eu>
2021-08-08 18:34:12 +0200

    Some formating and add usage

--- src/main/kotlin/com/github/njibhu/woodswap/SwapCommandListener.kt
+++ src/main/kotlin/com/github/njibhu/woodswap/SwapCommandListener.kt
@@ -11,7 +11,7 @@ class SwapCommandListener: CommandExecutor{
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if(args.size < 2){
             sender.sendMessage("Expecting two arguments")
-            return true
+            return false
         }
         val sourceWood = WoodType.values().find {
             it.type == args[0]
@@ -21,14 +21,14 @@ class SwapCommandListener: CommandExecutor{
         }
         if(sourceWood == null || targetWood == null){
             sender.sendMessage("Invalid wood type for command woodswap")
-            return true
+            return false
         }
 
         val worldEdit = WorldEdit.getInstance()
 
         if(sender !is org.bukkit.entity.Player) {
             sender.sendMessage("This command can only be ran by a player")
-            return  true
+            return false
         }
 
         val actor = BukkitAdapter.adapt(sender)
@@ -37,15 +37,16 @@ class SwapCommandListener: CommandExecutor{
         val selectionWorld = localSession.selectionWorld
 
         if (selection == null || selectionWorld == null){
-            sender.sendMessage("You need to have a selection first");
-            return true
+            sender.sendMessage("You need to have a selection first")
+            return false
         }
 
         runBlocking {
             launch {
+                var blocksChangedCount = 0
                 val editSession =  WorldEdit.getInstance().newEditSession(actor)
-                val sourceWoodBlocks = getBlockTypes(sourceWood.type);
-                val targetWoodBlocks = getBlockTypes(targetWood.type);
+                val sourceWoodBlocks = getBlockTypes(sourceWood.type)
+                val targetWoodBlocks = getBlockTypes(targetWood.type)
 
                 localSession.selection.forEach {
                     val block = selectionWorld.getBlock(it)
@@ -53,11 +54,12 @@ class SwapCommandListener: CommandExecutor{
                     if (index > -1){
                         val equivalentBlock = targetWoodBlocks[index]
                         editSession.setBlock(it, equivalentBlock.getState(block.states))
+                        blocksChangedCount += 1
                     }
                 }
                 editSession.close()
                 localSession.remember(editSession)
-                sender.sendMessage("/woodswap ${args[0]} ${args[1]} done")
+                sender.sendMessage("/woodswap ${args[0]} ${args[1]} done: $blocksChangedCount blocks swapped")
             }
         }
 
